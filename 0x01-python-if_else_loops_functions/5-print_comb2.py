6a225eeaa20305978697bbecea99e9fcd5d0dba3
ami_birhanu <lightened1@github.com>
2022-11-29 23:33:28 -0800

    efd

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-number=0
-while (number<=99):
-    if number!=99:
-        print("{:02d}, ".format(number),end=" ")
+number = 0
+while number <= 99:
+    if number != 99:
+        print("{:02d}, ".format(number), end='')
     else:
         print("{:02d}".format(number))
-    number=number + 1
+    number = number + 1
